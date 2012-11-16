da20cf3e4f192bb0bd7450125575a2d1cae12f26
StuFF mc <mc@stuffmc.com>
2012-11-16 09:39:32 +0100

    Initial Commit

---
+++ TreeBindings/AppDelegate.h
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.h
+//  TreeBindings
+//
+//  Created by StuFF mc on 11/16/12.
+//  Copyright (c) 2012 ObjCGN.com. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+
+- (IBAction)saveAction:(id)sender;
+
+@end
---
+++ TreeBindings/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  TreeBindings
+//
+//  Created by StuFF mc on 11/16/12.
+//  Copyright (c) 2012 ObjCGN.com. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
